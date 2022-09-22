45d67d356d15b2df024ee517de8f6d0fff5cd46d
alessandro <alessandro@pcdiale.fritz.box>
2022-09-22 16:27:55 +0200

    why

--- ElaboratoIdS/src/it/unibs/ids/elaborato/Articolo.java
+++ ElaboratoIdS/src/it/unibs/ids/elaborato/Articolo.java
@@ -1,5 +1,22 @@
 package it.unibs.ids.elaborato;
 
 public class Articolo {
+	private Categoria categoriaArticolo;
+	private StatiOfferta statoOfferta;
+	
+	public Articolo(Categoria categoriaArticolo, StatiOfferta statoOfferta) {
+		this.categoriaArticolo = categoriaArticolo;
+		this.statoOfferta = statoOfferta;
+	}
+	
+	
+	public Categoria getCategoriaArticolo() {
+		return categoriaArticolo;
+	}
 
+	public StatiOfferta getStatoOfferta() {
+		return statoOfferta;
+	}
+	
+	
 }
